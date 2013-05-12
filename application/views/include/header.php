85e78ceae54d446e242940d9a4698e764d50504c
Steven Lu <tacticalazn@gmail.com>
2013-05-12 17:28:20 -0400

    Fixing meta tags.

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -3,7 +3,7 @@
 <head>
    <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta name="description" content="" />
+   <meta name="description" content="Kevin is father, Senior Principal Scientist at Broadcom, and a member of IEEE-SA Standards Board." />
    <meta name="keywords" content="kevin lu, telcordia, broadcom, scientist, telecom, research" />
 
    <title>Kevin W. Lu</title>
